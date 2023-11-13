832b7a03780293859861c66b280bf1f78ac19b23
Kristopher Roy <kristopher.roy@gmail.com>
2023-11-13 14:42:32 -0800

    updated the get commands to user MGGraph API

--- O365-Azure/Azure-MemberUserReport.ps1
+++ O365-Azure/Azure-MemberUserReport.ps1
@@ -18,207 +18,32 @@ ELSEIF($curver -lt $ver){powershell -Command "& {[System.Reflection.Assembly]::L
 EXIT}
 
 #config file
-$scriptpath = "E:\Scripts\WeeklyReporting"
+$scriptpath = "C:\Projects\GTIL\Reports\CreateTabbed-Weekly-Report"
 [xml]$cfg = Get-Content $scriptpath"\RptCFGFile.xml"
-
-$GTILCred = Import-Clixml "C:\scripts\CRD\GTILCred.xml"
-#$BTLCred = Import-Clixml "C:\Projects\CRD\BTLCred.xml"
-
-connect-azuread -Credential $GTILCred
-
-#Timestamp
-$runtime = Get-Date -Format "yyyyMM"
-
-#folder to store completed reports
-$rptfolder = "C:\Reports\GTIL\"
-
-$mailParams = @{
-    SmtpServer                 = $cfg.Settings.EmailSettings.SMTPServer
-    From                       = $cfg.Settings.EmailSettings.FromAddress
-    To                         = ($cfg.Settings.EmailSettings.ToAddress).split(",")
-    Subject                    = "GTIL Monthly - "+$runtime+"-UserList"
-    Body                       = "This report contains a monthly automated output of O365/Azure user lists"
-    Attachments = $rptFolder+$runtime+'-UserList.csv'
-    DeliveryNotificationOption = 'OnFailure', 'OnSuccess'
-}
-
-Get-AzureADUser -all $true | select DisplayName,JobTitle,Mail,UserType|export-csv $rptFolder$runtime-UserList.csv -NoTypeInformation
-Send-MailMessage @mailParams
-
-Send-MailMessage -SmtpServer 'smtp.belltechlogix.com' -from 'Reports@belltechlogix.com' -to 'kroy@belltechlogix.com' -Subject 'Test'
-
-
-
-
-
-
-
-
-<# For historical
-Connect-AzureAD
-
-#config file
-$scriptpath = "E:\Scripts\WeeklyReporting"
-[xml]$cfg = Get-Content $scriptpath"\RptCFGFile.xml"
-
 #Organization that the report is for
 $org = $cfg.Settings.DefaultSettings.OrgName
-
-#modify this for your searchroot can be as broad or as narrow as you need down to OU
-$domainRoot = $cfg.Settings.DefaultSettings.DomainRoot
-$DC1 = $cfg.Settings.DefaultSettings.DC
-
-#If multiple domains uncoment and use
-#$domainRoot2 = $cfg.Settings.DefaultSettings.DomainRoot2
-#$DC2 = $cfg.Settings.DefaultSettings.DC2
-
-#folder to store completed reports
-$rptfolder = $cfg.Settings.DefaultSettings.ReportFolder
-
-#mail recipients for sending report
-$recipients = @("Kristopher <kroy@belltechlogix.com>","John <JTapscott@belltechlogix.com>","Tim <twheeler@belltechlogix.com>")
-
-#from address
-$from = $cfg.Settings.EmailSettings.FromAddress
-
-#smtpserver
-$smtp = $cfg.Settings.EmailSettings.SMTPServer
+$tenant = $cfg.Settings.DefaultSettings.TenantID
+# Read the credentials
+$Credentials = Import-Clixml -Path $scriptpath\O365-Azure\Access.xml
 
 #Timestamp
 $runtime = Get-Date -Format "yyyyMMMdd"
 
-#deffinition for UAC codes
-$lookup = @{4096="Workstation/Server"; 4098="Disabled Workstation/Server"; 4128="Workstation/Server No PWD"; 
-4130="Disabled Workstation/Server No PWD"; 528384="Workstation/Server Trusted for Delegation";
-528416="Workstation/Server Trusted for Delegation"; 532480="Domain Controller"; 66176="Workstation/Server PWD not Expire"; 
-66178="Disabled Workstation/Server PWD not Expire";512="User Account";514="Disabled User Account";66048="User Account PWD Not Expire";66050="Disabled User Account PWD Not Expire"}
+#folder to store completed reports
+$rptfolder = $cfg.Settings.DefaultSettings.ReportFolder
 
-#creates the report folder if it doesn't exist
-if(!(Test-Path -Path $rptfolder)){
-    New-Item -ItemType directory -Path $rptfolder
-}
+#Disconnect-MgGraph
+disconnect-mggraph
+#connect-MgGraph
+connect-MgGraph -TenantId $tenant -clientsecretcredential $credentials
 
-$qadusers = get-qaduser -searchroot $domainRoot -Service $DC1 -searchscope subtree -sizelimit 0 -includedproperties displayname,SamAccountName,givenName,sn,UserPrincipalName,memberof,telephoneNumber,mobile,mail,userAccountControl,whenCreated,whenChanged,lastlogondate,dayssincelogon,lastlogontimestamp,employeetype,description,office,City,cn,badPasswordTime,pwdLastSet,LockedOut,accountExpires,ProxyAddresses|Select-Object -Property displayname,SamAccountName,givenName,sn,UserPrincipalName,@{N='Domain';E={("$_.domain".split('\')[0])}},@{N='OU';E={($_.canonicalName -split('/')|select-object -skip 1 -Last 10) -Join "/"}},lastlogontimestamp,@{N='dayssincelogon';E={(new-timespan -start (get-date $_.LastLogonTimestamp -Hour "00" -Minute "00") -End (get-date -Hour "00" -Minute "00")).Days}},employeetype,@{N='userAccountControl';E={$lookup[$_.userAccountControl]}},@{N='Groups';E={[system.String]::Join(", ", ($_.memberof|get-adgroup|select name -expandproperty name))}},telephoneNumber,mobile,mail,whenCreated,whenChanged,description,office,City,badPasswordTime,pwdLastSet,LockedOut,accountExpires,@{N='ProxyAddresses';E={[system.String]::Join(", ", ($_.ProxyAddresses))}}|sort sn
-#If multiple domains uncomment and use
-#$qadusers += get-qaduser -searchroot $domainRoot2 -Service $DC2 -searchscope subtree -sizelimit 0 -includedproperties displayname,SamAccountName,givenName,sn,UserPrincipalName,memberof,telephoneNumber,mobile,mail,userAccountControl,whenCreated,whenChanged,lastlogondate,dayssincelogon,lastlogontimestamp,employeetype,description,office,City,cn,badPasswordTime,pwdLastSet,LockedOut,accountExpires,ProxyAddresses|Select-Object -Property displayname,SamAccountName,givenName,sn,UserPrincipalName,@{N='Domain';E={("$_.domain".split('\')[0])}},@{N='OU';E={($_.canonicalName -split('/')|select-object -skip 1 -Last 10) -Join "/"}},lastlogontimestamp,@{N='dayssincelogon';E={(new-timespan -start (get-date $_.LastLogonTimestamp -Hour "00" -Minute "00") -End (get-date -Hour "00" -Minute "00")).Days}},employeetype,@{N='userAccountControl';E={$lookup[$_.userAccountControl]}},@{N='Groups';E={[system.String]::Join(", ", ($_.memberof|get-adgroup|select name -expandproperty name))}},telephoneNumber,mobile,mail,whenCreated,whenChanged,description,office,City,badPasswordTime,pwdLastSet,LockedOut,accountExpires,@{N='ProxyAddresses';E={[system.String]::Join(", ", ($_.ProxyAddresses))}}|sort sn
+$Users = Get-MgUser -all -filter "UserType eq 'Member'" -Select UserType,SignInActivity,AccountEnabled,City,CompanyName,Country,Department,DisplayName,GivenName,SurName,ID,JobTitle,Mail,MemberOf,State,UserPrincipalName,LastLogonTimestamp,SecurityIdentifier | select-object *,@{N='LastLogonTimestamp';E={ [datetime]$_.SignInActivity.LastSignInDateTime}},@{N='LastNonInteractiveTimestamp';E={ [datetime]$_.SignInActivity.LastNonInteractiveSignInDateTime}}
 
+$users|select DisplayName,@{N='SID';E={($_.SecurityIdentifier)}},givenName,surName,UserPrincipalName,@{N='Domain';E={($_.UserPrincipalName.split('@')[1])}},Department,LastLogonTimestamp,@{N='dayssincelogon';E={(new-timespan -start (get-date $_.LastLogonTimestamp -Hour "00" -Minute "00") -End (get-date -Hour "00" -Minute "00")).Days}},LastNonInteractiveTimestamp,JobTitle,ID,Groups|export-csv $rptFolder$runtime-MemberUserReport.csv -NoTypeInformation
 
-$qadusers|export-csv $rptFolder$runtime-qADUserReport.csv -NoTypeInformation
-
-
-#If Running standalone un-comment to send mail
-<#
-	$usercount = $qadusers.Count
-
-	$emailBody = "<h1>$org Weekly ADUser Report</h1>"
-	$emailBody = $emailBody + "<h2>$org ADUser Count - '$usercount'</h2>"
-	$emailBody = $emailBody + "<p><em>"+(Get-Date -Format 'MMM dd yyyy HH:mm')+"</em></p>"
-
-	$htmlforEmail = $emailBody + @'
-	<h3>Included Fields:</h3>
-	<table style="height: 535px;" border="1" width="625">
-	<tbody>
-	<tr>
-	<td style="width: 304px;"><strong>displayname</strong></td>
-	<td style="width: 305px;">&nbsp;</td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>SamAccountName</strong></td>
-	<td style="width: 305px;"><em>System Name used by Active Directory</em></td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>givenName</strong></td>
-	<td style="width: 305px;"><em>First Name</em></td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>sn</strong></td>
-	<td style="width: 305px;"><em>Last Name</em></td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>UserPrincipalName</strong></td>
-	<td style="width: 305px;"><em>System Name used by multiple platforms</em></td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>lastlogontimestamp</strong></td>
-	<td style="width: 305px;"><em>if available, last time AD recorded login</em></td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>dayssincelogon</strong></td>
-	<td style="width: 305px;"><em>calculated from lastlogontimestamp</em></td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>employeetype</strong></td>
-	<td style="width: 305px;">&nbsp;</td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>userAccountControl</strong></td>
-	<td style="width: 305px;"><em>User settings for AD</em></td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>Groups</strong></td>
-	<td style="width: 305px;"><em>AD Groups user is member of</em></td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>telephoneNumber</strong></td>
-	<td style="width: 305px;">&nbsp;</td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>mobile</strong></td>
-	<td style="width: 305px;">&nbsp;</td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>mail</strong></td>
-	<td style="width: 305px;">&nbsp;</td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>whenCreated</strong></td>
-	<td style="width: 305px;"><em>When account was created</em></td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>whenChanged</strong></td>
-	<td style="width: 305px;"><em>Date AD changes were made to account</em></td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>description</strong></td>
-	<td style="width: 305px;">&nbsp;</td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>office</strong></td>
-	<td style="width: 305px;">&nbsp;</td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>City</strong></td>
-	<td style="width: 305px;">&nbsp;</td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>badPasswordTime</strong></td>
-	<td style="width: 305px;"><em>Last time password was typed incorrectly</em></td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>pwdLastSet</strong></td>
-	<td style="width: 305px;"><em>Last time password was reset</em></td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>LockedOut</strong></td>
-	<td style="width: 305px;"><em>account lockout details</em></td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>accountExpires</strong></td>
-	<td style="width: 305px;"><em>Date Account expires if set</em></td>
-	</tr>
-	<tr>
-	<td style="width: 304px;"><strong>ProxyAddresses</strong></td>
-	<td style="width: 305px;"><em>email addresses for account</em></td>
-	</tr>
-	</tbody>
-	</table>
-	'@
-
-	Send-MailMessage -from $from -to $recipients -subject "$org - AD User Report" -smtpserver $smtp -BodyAsHtml $htmlforEmail -Attachments $rptFolder$runtime-qADUserReport.csv
-#>
 
 #cleanup old coppies
 $Daysback = '-14'
 $CurrentDate = Get-Date
 $DateToDelete = $CurrentDate.AddDays($Daysback)
-Get-ChildItem $rptFolder | Where-Object { $_.LastWriteTime -lt $DatetoDelete -and $_.Name -like "*qADUserReport*"} | Remove-Item
+Get-ChildItem $rptFolder | Where-Object { $_.LastWriteTime -lt $DatetoDelete -and $_.Name -like "*MemberUserReport*"} | Remove-Item
